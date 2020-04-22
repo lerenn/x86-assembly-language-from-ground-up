00e4032ba299d73c5ccbc7f026d782fdad8f52ad
Louis FRADIN <louis.fradin@gmail.com>
2020-04-22 11:52:05 +0200

    Backward Compat: complete exercice

--- BackwardCompat/main.asm
+++ BackwardCompat/main.asm
@@ -1,6 +1,7 @@
 ; MASM
 ; ----------------------------------------------------------------------------------------
 
+; x86-64bit-------------------------------------------------------------------------------
 ; .code
 ; main  proc
 ;       mov rax,2
@@ -10,5 +11,57 @@
 ; main  endp
 ; end
 
+; x86-32bit-------------------------------------------------------------------------------
+; .386
+; .model flat
+; .code
+; main  proc
+;       mov eax,2
+;       mov ebx,3
+;       mov ecx,4
+;       ret
+; main  endp
+; end
+
+; 16bit-----------------------------------------------------------------------------------
+; .386
+; .model flat
+; .code
+; main  proc
+;       mov ax,2
+;       mov bx,3
+;       mov cx,4
+;       ret
+; main  endp
+; end
+
+
 ; NASM
 ; ----------------------------------------------------------------------------------------
+
+        global  _start
+
+        section .text
+_start:
+        ; This section is a bit modified to highlight modification by using different modes
+        ; There should be remaining bits from larger modes  
+
+        ; x86-64bit
+        mov rax,0x1111111111111111
+        mov rbx,0x2222222222222222
+        mov rcx,0x3333333333333333
+
+        ; x86-32bit
+        mov eax,0x44444444
+        mov ebx,0x55555555
+        mov ecx,0x66666666
+
+        ; ; 16bit
+        mov ax,0x7777
+        mov bx,0x8888
+        mov cx,0x9999
+        
+        ; Exit program
+        mov     eax, 60                 ; system call for exit
+        xor     edi, edi                ; exit code 0
+        syscall                         ; invoke operating system to exit
--- BackwardCompat/main.gdb
+++ BackwardCompat/main.gdb
@@ -1,2 +1,17 @@
 # Set gdb
 set breakpoint pending on
+
+# Set breakpoints
+b main.asm:65
+
+# Run program
+r
+
+# Display informations
+info registers rax
+info registers rbx
+info registers rcx
+
+# Stop and quit
+kill
+q
